5b67ae5d86e8476c889bbf74ae50095e7182ad3b
Jo Colina <jo@jocolina.com>
2015-04-27 16:30:51 -0400

    Minor changes, creation of miniNotif variable

--- miniNotif.js
+++ miniNotif.js
@@ -7,73 +7,74 @@
 				2015
 ***************************************/
 
-function notification(title, options){
-	
 
-	this.title = title;
+var miniNotif = {
 
-	this.show = function(){
-		var returnTemp;
-		if (! ("Notification" in window)){
-			returnTemp = {
-				code : 001,
-				message: 'Notifications unexistant or not enabled'
-			};
 
-			return returnTemp;
+	notification: function(title, options){
+		
 
-		}else if (Notification.permission === "granted") {
-			var notif = new Notification(this.title, options);
+		this.title = title;
 
-			returnTemp = {
-				code: 002,
-				message: "Notification set and Shown"
-			};
-			return returnTemp;
+		this.show = function(){
+			var returnTemp;
+			if (! ("Notification" in window)){
+				returnTemp = {
+					code : 001,
+					message: 'Notifications unexistant or not enabled'
+				};
 
-		}else if(Notification.permission !== 'denied'){
-			Notification.requestPermission(function (permission){
-				if (permission === "granted"){
+				return returnTemp;
 
-					var notif = new Notification(this.title, options);
+			}else if (Notification.permission === "granted") {
+				var notif = new Notification(this.title, options);
 
-					returnTemp = {
-						code: 003,
-						message: "Notification set and Shown (Chrome)"
-					};
-					return returnTemp;
-				}else{
-					returnTemp={
-						code : 004,
-						message: "Notifications permission denied"
-					};
-					return returnTemp;
-				}
-			});
+				returnTemp = {
+					code: 002,
+					message: "Notification set and Shown"
+				};
+				return returnTemp;
 
-		}else if (Notification.permission === "denied"){
-			returnTemp={
-				code : 004,
-				message: "Notifications permission denied"
-			};
-			return returnTemp;
+			}else if(Notification.permission !== 'denied'){
+				Notification.requestPermission(function (permission){
+					if (permission === "granted"){
+
+						var notif = new Notification(this.title, options);
+
+						returnTemp = {
+							code: 003,
+							message: "Notification set and Shown (Chrome)"
+						};
+						return returnTemp;
+					}else{
+						returnTemp={
+							code : 004,
+							message: "Notifications permission denied"
+						};
+						return returnTemp;
+					}
+				});
+
+			}else if (Notification.permission === "denied"){
+				returnTemp={
+					code : 004,
+					message: "Notifications permission denied"
+				};
+				return returnTemp;
+			}
+
 		}
 
-	}
-
-	
+	},
 
 
-}
-
-var miniNotif={
 	new: function(title, body, icon){
-		new notification(title, {body: body || null, icon: icon || null}).show();
+		new this.notification(title, {body: body || null, icon: icon || null}).show();
 	},
 
 	withId: function(id, title, body, icon){
 		if (typeof id == "undefined") throw new Error("Id is required when calling miniNotif.withId");
-		new notification(title, {body: body || null, icon: icon ||null, tag: id}).show();
+		new this.notification(title, {body: body || null, icon: icon ||null, tag: id}).show();
 	}
 
 
